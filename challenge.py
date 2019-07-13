a303e4a88ed449a6892d58e07beb743dee68e547
Chris Helmich <kagekirin@gmail.com>
2019-07-13 16:44:55 +0900

    Add challenge.py (fully implemented)

---
+++ challenge.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python3
+
+import os, sys, time, random, argparse
+
+
+def create_exercise(args):
+    a = random.randint(0, args.range)
+    b = random.randint(0, args.range)
+    if args.negative and args.operand == '-':
+        b = random.randint(a, args.range)
+    
+    return "{} {} {}".format(a, args.operand, b)
+
+def eval_exercise(computation, result):
+    rr = eval(computation)
+    print(rr)
+    if rr == int(result):
+        return True
+    return False
+
+def main(args):
+    count_exe = 0
+    count_good = 0
+    count_bad = 0
+
+    end_timer = time.time() + args.time * 10
+
+    while time.time() < end_timer:
+        count_exe += 1
+        exe = create_exercise(args)
+        print(exe)
+        result = input('> ') or 0
+        ok = eval_exercise(exe, result)
+        if ok:
+            count_good += 1
+        else:
+            count_bad += 1
+        print(ok)
+    
+    print("exercises:", count_exe)
+    print("good:", count_good, "/", count_exe)
+    print("bad:", count_bad, "/", count_exe)
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--time', help='time (min)', type=int, default=1)
+    parser.add_argument('-o', '--operand', help='operand', choices=['+', '-'], type=str, default='+')
+    parser.add_argument('-r', '--range', help='range', type=int, default=10)
+    parser.add_argument('--negative', help='allow negatives', action='store_true')
+    args = parser.parse_args()
+    random.seed()
+    main(args)
